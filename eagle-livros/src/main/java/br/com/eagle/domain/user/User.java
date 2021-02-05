ca53e3435d3ba727900bc7b9229dd849f45dc037
GeorgeHenriqueGH <georgehenriquecampos@gmail.com>
2021-02-05 19:23:49 -0300

    Configurando o Lombok e JPA

--- eagle-livros/src/main/java/br/com/eagle/domain/user/User.java
+++ eagle-livros/src/main/java/br/com/eagle/domain/user/User.java
@@ -2,13 +2,26 @@ package br.com.eagle.domain.user;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 import lombok.EqualsAndHashCode;
 
 @Getter
 @Setter
-@EqualsAndHashCode
-public abstract class User {
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
+@Entity	
+@SuppressWarnings("serial")
+@MappedSuperclass
+public abstract class User implements Serializable {
 	
+	@EqualsAndHashCode.Include
+	@Id
 	private Integer id;
 	
 	private String nome;
